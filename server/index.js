6f928dd3db51060888f5e1a760b76289e22173de
Ketul Patel <ketulp19120@gmail.com>
2025-07-05 16:20:55 -0400

    - Updated auth routes. - Removed registration logic.

--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
-const express = require("express");
-const cors = require("cors");
 require("dotenv").config();
 
+const express = require("express");
+const path = require("path");
+const authRoutes = require("./routes/auth")
+
 const app = express();
-app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req,res)=>{
     res.send("EHR is running!")
 })
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('Server is running on port ${PORT}'));
+app.use("/auth", authRoutes)
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 const { PrismaClient } = require("@prisma/client");
+const validateCredentials = require("./middleware/validateCredentials")
+
 const router = express.Router();
 const prisma = new PrismaClient();
 
+/*
+//keeping the registeration code for future use
 router.post("/register", async (req, res) => {
   const { email, password, role } = req.body;
   const hashed = await bcrypt.hash(password, 10);
@@ -17,17 +20,20 @@ router.post("/register", async (req, res) => {
     res.status(400).json({ error: "Email already exists" });
   }
 });
+*/
 
-router.post("/login", async (req, res) => {
+router.use(express.json());
+router.use(express.urlencoded({extended: true}));
+
+router.post("/login", validateCredentials, async (req, res) => {
   const { email, password } = req.body;
   const user = await prisma.user.findUnique({ where: { email } });
+
   if (!user || !(await bcrypt.compare(password, user.password))) {
     return res.status(401).json({ error: "Invalid credentials" });
   }
-  const token = jwt.sign({ userId: user.id, role: user.role }, process.env.JWT_SECRET, {
-    expiresIn: "1h"
-  });
-  res.json({ token });
+  
+  res.send("Login Successfull!");
 });
 
 module.exports = router;
