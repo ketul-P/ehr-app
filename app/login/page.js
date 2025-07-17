d54576c2bf11d8b32dc64396b6684bb5ded28c62
Ketul Patel <ketulp19120@gmail.com>
2025-07-17 00:49:51 -0400

    - Added additional buttons - sing up, and secondary actios

--- app/login/page.js
+++ app/login/page.js
@@ -2,9 +2,9 @@ export default function Login() {
   return (
     <main id="form" className="min-h-screen bg-white flex items-center p-4 rounded">
       
-      <section className= "p-8 border border-gray-300 shadow-md rounded-xl max-w-xl w-full text-left">
+      <section className= "p-8 border border-gray-300 shadow-2xl rounded-xl max-w-xl w-full text-left">
         
-        <h1 className="text-3xl font-bold text-blue-600 mb-1">Welcome!</h1>
+        <h1 className="text-3xl font-bold text-blue-500 mb-1">Welcome!</h1>
         
         <p className="text-gray-600 leading-relaxed mb-4">
           Enter your credentials to sign in, or <span className="text-blue-500 underline hover:cursor-pointer ">sign up</span> to get started.
@@ -30,9 +30,12 @@ export default function Login() {
               placeholder="Enter your username"/>
           </label>
 
-          
+          <div className="grid grid-cols-2 gap-x-4">
+            <label htmlFor="remember" className="text-gray-800 text-left">Remember Me<input type="checkbox" className="m-2"></input></label>
+            <label htmlFor="forgot" className="text-blue-500 text-right underline hover:cursor-pointer">Forgot password</label>
+          </div>
 
-          <button type="submit" className="w-full mt-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
+          <button type="submit" className="w-full mt-4 bg-teal-500 hover:bg-teal-600 text-white font-semibold py-2 px-4 rounded">
             Submit
           </button>
       </section>
