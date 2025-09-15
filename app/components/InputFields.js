3d10f4e5fdc1ff9870b7d2bd01fd9e00843cf3e0
Ketul Patel <ketulp19120@gmail.com>
2025-09-15 16:35:08 -0400

    - pre cleanup for vercel migration

--- app/components/InputFields.js
+++ app/components/InputFields.js
@@ -5,19 +5,20 @@ import React from "react";
 
 // Create basic input field with custom focus color and placeholder. 
 // Make sure to add a name
-export function InputBasic({name, type, placeHolder, focusOutline}){
+export function InputBasic({name, type, placeHolder, focusOutline, ...props}){
     return (
         <input 
                 name=        {name}
                 type=        {type} 
                 placeholder= {placeHolder}
                 className=   {`border bg-white text-gray-800 border-gray-300 rounded-md  focus:outline-none focus:border-${focusOutline} p-2`}
+                {...props}
         />
     );
 }
 
 // Provides the same utility as InputBasic for mandatory fields.
-export function InputBasicRequired({name, type, placeHolder, focusOutline}){
+export function InputBasicRequired({name, type, placeHolder, focusOutline, ...props}){
     return (
         <input 
                 name=        {name} 
@@ -25,28 +26,32 @@ export function InputBasicRequired({name, type, placeHolder, focusOutline}){
                 placeholder= {placeHolder}
                 className=   {`border bg-white text-gray-800 border-gray-300 rounded-md w-full focus:outline-none focus:border-${focusOutline} p-2`}
                 required
+                {...props}
         />
     );
 }
 
-export function InputBasicDisabled(name, placeholder, focusOutline){
+export function InputBasicDisabled(name, placeholder, focusOutline, ...props){
     return (
         <input 
                 name={name} 
                 placeholder={placeholder}
                 disabled
                 className=  "border bg-white text-gray-800 border-gray-300 rounded-md focus:outline-none focus:{focusOutline} p-2" 
+                {...props}
         />
     );
 }
 
-export function InputBasicDisabledRequired(name, placeholder, focusOutline){
+export function InputBasicDisabledRequired(name, placeholder, focusOutline, ...props){
     return (
         <input 
                 name={name} 
                 placeholder={placeholder}
                 disabled
                 className=   "border bg-white text-gray-800 border-gray-300 rounded-md focus:outline-none focus:{focusOutline} p-2" 
-        required />
+                required
+                {...props}
+        />
     );
 }
--- app/login/page.js
+++ app/login/page.js
@@ -1,85 +1,89 @@
 "use client";
 
+import { useState } from "react"
 import { InputBasicRequired } from "../components/inputFields";
 import { InputBasic } from "../components/inputFields";
 import { PrimaryButton } from "../components/customButtons";
 
 export default function LoginForm() {
 
-      const [username, setUsername] = useState("");
-      const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-      async function handleSubmit(e) {
-        e.preventDefault(); // prevent page reload
+    async function handleSubmit(e) {
+      e.preventDefault(); // prevent page reload
 
-        const res = await fetch("/api/login", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ username, password }),  // <-- format check here
-        });
+      const res = await fetch("/api/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }), 
+      });
 
-        const data = await res.json();
-        console.log("API response:", data);
-      }
+      const data = await res.json();
+      console.log("API response:", data);
+    }
 
-  return (
-    <main id="form" className="min-h-screen bg-white flex justify-center font-mono">
-      <div id="login-form" className = "flex items-center">
-        <section className= "p-10 grid grid-rows-[auto_auto_auto_auto_auto] gap-2 bg-emerald-50 max-w-md text-left rounded-2xl border border-emerald-100 shadow-xl">
-        
-          <div>
-            {/* Title */}
-            <h1 className="text-4xl text-emerald-500 mb-1">Welcome!</h1>
-            {/* Disclaimer */}
-            <p className="text-gray-400 leading-relaxed text-xs mb-12">
-              Enter your credentials to sign in, or <span className="text-emerald-700 underline hover:cursor-pointer ">sign up</span> to get started.
-            </p>
-          </div>
+    return (
+      <main id="form" className="min-h-screen bg-white flex justify-center font-mono">
+        <div id="login-form" className = "flex items-center">
+          <section className= "p-10 grid grid-rows-[auto_auto_auto_auto_auto] gap-2 bg-emerald-50 max-w-md text-left rounded-2xl border border-emerald-100 shadow-xl">
           
-          <div className="w-full border">
-            <InputBasicRequired
-                name="username"
-                type="text"
-                placeHolder="Username"
-                focusOutline="emerald-400"
-            />
-          </div>
-
-          <div>
-            <InputBasicRequired
-              name="password"
-              type="text"
-              placeHolder="Password"
-              focusOutline="emerald-400"
-              
-            />
-          </div>
-          
-          <div className="grid grid-cols-2 text-0.5rem gap-x-4 mt-2">
-            <label htmlFor="remember" className="text-gray-500 text-left pr-4">
-              Remember Me
-
-              <InputBasic
-                name="rememberUser"
-                type="checkbox"
-                placeHolder=""
-                focusOutline="emerald-400"
-
-              />
-            </label>                
+            <div>
+              {/* Title */}
+              <h1 className="text-4xl text-emerald-500 mb-1">Welcome!</h1>
+              {/* Disclaimer */}
+              <p className="text-gray-400 leading-relaxed text-xs mb-12">
+                Enter your credentials to sign in, or <span className="text-emerald-700 underline hover:cursor-pointer ">sign up</span> to get started.
+              </p>
+            </div>
             
-            <label htmlFor="forgot" className="text-indigo-500 text-right underline hover:cursor-pointer">Forgot password</label>
-          </div>
+            <div className="w-full border">
+                <InputBasicRequired
+                  name="username"
+                  type="text"
+                  placeHolder="Username"
+                  focusOutline="emerald-400"
+                  value={username}
+                  onChange={(e)=> setUsername(e.target.value)}
+                />
+            </div>
 
-          <div className="mt-4">
-            <PrimaryButton
-              value="Submit"
-            />
-          </div>
+            <div>
+                <InputBasicRequired
+                  name="password"
+                  type="text"
+                  placeHolder="Password"
+                  focusOutline="emerald-400"
+                  value={username}
+                  onChange={(e)=> setUsername(e.target.value)}
+                />
+            </div>
+            
+            <div className="grid grid-cols-2 text-0.5rem gap-x-4 mt-2">
+              <label htmlFor="remember" className="text-gray-500 text-left pr-4">
+                Remember Me
 
+                <InputBasic
+                  name="rememberUser"
+                  type="checkbox"
+                  placeHolder=""
+                  focusOutline="emerald-400"
 
-        </section>
-      </div>
-    </main>
-  );
+                />
+              </label>                
+              
+              <label htmlFor="forgot" className="text-indigo-500 text-right underline hover:cursor-pointer">Forgot password</label>
+            </div>
+
+            <div className="mt-4">
+              <PrimaryButton
+                value="Submit"
+              />
+            </div>
+
+  
+          </section>
+        </div>
+      </main>
+    );
 }
